780880bed1f4aa57a7a4da0aa73bdf17f81117da
Chris Gibson <gibs@Chriss-MacBook-Air.local>
2022-09-19 20:02:25 +0800

    New image picker icon with box shadow, guess the color

--- src/pages/CreateEvent/CreateEventPage.jsx
+++ src/pages/CreateEvent/CreateEventPage.jsx
@@ -10,15 +10,14 @@ import './CreateEventPage.css'
 import {
   Link
 } from "react-router-dom";
-import { ImCalendar, ImLocation, ImImage } from "react-icons/im";
+import { ImImage } from "react-icons/im";
 import { IconContext } from "react-icons";
-import { red } from '@mui/material/colors';
 
 const CreateEventPage = () => {
 	const [eventDetails, setEventDetails] = useState({
 		eventName: '',
 		hostName: '',
-		eventPhoto: BirthdayCake,
+		eventPhoto: null,
 		location: {
 			streetName: '',
 
@@ -26,15 +25,17 @@ const CreateEventPage = () => {
 	})
 
 	const onChangeHandler = (event) => {
+		if(event.target.files && event.target.files[0]) {
+			console.log(event.target.files)
+
+		}
 		console.log('event: ',event)
 		setEventDetails(currentState => ({...currentState, [event.target.name]: event.target.value}))
-		// if(event.target.value) imageUrl()
 	}
 
-	// const imageUrl = () => {
-	// 	URL.createObjectURL(eventDetails.eventPhoto)
-	// }
-	//onClick={goToEventPage}
+	const changeImage = (event) => {
+		URL.createObjectURL(eventDetails.eventPhoto)
+	}
 
 	const changeDateValue = (value, keyName) => {
 		setEventDetails(currentState => ({...currentState, [keyName]: value}))
@@ -43,16 +44,28 @@ const CreateEventPage = () => {
 	return (
 		<div className="container">
 			<div className="imageContainer">
-				<img src={eventDetails.eventPhoto || BirthdayCake} alt="" className="eventImage"/>
-				<label for="imgUpload" className="imagePicker">
-				<IconContext.Provider value={{ color: "#8456EC", className: "global-class-name", size: '100px'}}>
-					<div>
-						<ImImage/>
-						<input type="file" name="photo" id="imgUpload" onChange={onChangeHandler} hidden/>
-					</div>
-				</IconContext.Provider>
-					<input type="file" name="photo" id="imgUpload" onChange={onChangeHandler} hidden/>
-				</label>
+
+
+					{eventDetails.eventPhoto
+					?
+						<label for="imgUpload">
+							<img src={eventDetails.eventPhoto || BirthdayCake} alt="event" className="eventImage"/>
+						</label>
+					:
+					// <label className="imagePicker">
+					<label for="imgUpload">
+						<div className="boxShadow">
+							<IconContext.Provider value={{ color: "#8456EC", className: "global-class-name", size: '80px',}}>
+								<div>
+									<ImImage/>
+
+								</div>
+							</IconContext.Provider>
+						</div>
+						</label>
+					}
+					<input type="file" name="eventPhoto" id="imgUpload" onChange={onChangeHandler} hidden/>
+
 			</div>
 
 			<TextField name="eventName" label="Title" onChange={onChangeHandler}/>
@@ -61,21 +74,21 @@ const CreateEventPage = () => {
 
 			<div className="dates">
 				<p className="startLabel">When: </p>
-				<p className="startLabel">Start</p>
 				<LocalizationProvider dateAdapter={AdapterDayjs}>
 					<DateTimePicker
 						renderInput={(props) => <TextField {...props} />}
 						name="startDate"
+						label="From"
 						value={eventDetails.startDate}
 						onChange={value => changeDateValue(value, 'startDate')}
 					/>
 				</LocalizationProvider>
 
-				<p className="endLabel">End</p>
 				<LocalizationProvider dateAdapter={AdapterDayjs}>
 					<DateTimePicker
 						renderInput={(props) => <TextField {...props} />}
 						name="endDate"
+						label="To"
 						value={eventDetails.endDate}
 						onChange={value => changeDateValue(value, 'endDate')}
 					/>

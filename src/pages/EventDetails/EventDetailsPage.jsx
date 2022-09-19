d990efc39eb241ac917e86a37bbb55eea926921f
Chris Gibson <gibs@Chriss-MacBook-Air.local>
2022-09-19 18:36:24 +0800

    Minor issue with dates when empty on events page

--- src/pages/EventDetails/EventDetailsPage.jsx
+++ src/pages/EventDetails/EventDetailsPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {useLocation} from 'react-router-dom';
 import './EventDetailsPage.css'
-import BirthdayCake from '../../assets/images/Birthday cake.png'
 import { ImCalendar, ImLocation } from "react-icons/im";
+import { IconContext } from "react-icons";
 
 /*
 
@@ -14,22 +14,32 @@ const EventDetailsPage = () => {
 	console.log('data: ', data);
 
 	const {eventName, hostName, startDate, endDate, eventLocation, eventPhoto} = data
-	console.log('date: ', JSON.stringify(startDate.$d))
+	console.log('date: ', JSON.stringify(startDate?.$d))
 	return (
 		<div className="eventContainer">
-			<img src={eventPhoto} alt="event image" className="eventImage"/>
+			<img src={eventPhoto} alt="event" className="eventImage"/>
 			<div className="alignLeft">
 				<p className="eventName">{eventName}</p>
 				<p>Hosted by: <span className="darker">{hostName}</span></p>
 			</div>
 				<div className="card">
-					<ImCalendar/>
-					<p className="purple">{JSON.stringify(startDate.$d)}</p>
+				<IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+					<div>
+						<ImCalendar/>
+					</div>
+				</IconContext.Provider>
+
+					<p className="purple">{JSON.stringify(startDate?.$d)}</p>
 					<p>to: <span className="darker">{JSON.stringify(endDate?.$d)}</span></p>
 				</div>
 
 				<div className="card">
-					<ImLocation />
+				<IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+					<div>
+						<ImLocation />
+					</div>
+				</IconContext.Provider>
+
 					<p className="purple">{eventLocation}</p>
 					{/* <p>{`${eventLocation.suburb} ${eventLocation.state} ${eventLocation.postcode}`}</p> */}
 				</div>
